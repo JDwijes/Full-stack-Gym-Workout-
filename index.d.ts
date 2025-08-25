b4652480d6a6cd7b6dac0e7758988dc1f65c8768
Janith Wijesingha Dinushika <104029903+JDwijes@users.noreply.github.com>
2025-08-25 10:00:56 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-declare function hasNativeSymbols(): boolean;
+declare function hasOwn<O, K extends PropertyKey, V = unknown>(o: O, p: K): o is O & Record<K, V>;
 
-export = hasNativeSymbols;
+export = hasOwn;
--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 'use strict';
 
-var test = require('tape');
-var hasSymbols = require('../');
-var runSymbolTests = require('./tests');
+var call = Function.prototype.call;
+var $hasOwn = Object.prototype.hasOwnProperty;
+var bind = require('function-bind');
 
-test('interface', function (t) {
-	t.equal(typeof hasSymbols, 'function', 'is a function');
-	t.equal(typeof hasSymbols(), 'boolean', 'returns a boolean');
-	t.end();
-});
-
-test('Symbols are supported', { skip: !hasSymbols() }, function (t) {
-	runSymbolTests(t);
-	t.end();
-});
-
-test('Symbols are not supported', { skip: hasSymbols() }, function (t) {
-	t.equal(typeof Symbol, 'undefined', 'global Symbol is undefined');
-	t.equal(typeof Object.getOwnPropertySymbols, 'undefined', 'Object.getOwnPropertySymbols does not exist');
-	t.end();
-});
+/** @type {import('.')} */
+module.exports = bind.call(call, $hasOwn);
